4eb12cd45886593e06e62579ab4639f831c154c2
Stevie Goodman <mail@steviegoodman.com>
2025-02-05 12:27:43 +0000

    Updated template

--- tests.server.lua
+++ tests.server.lua
@@ -3,8 +3,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TableUtil = require(ReplicatedStorage.DevPackages.TableUtil)
 local TestEZ = require(ReplicatedStorage.DevPackages.TestEZ)
 
-local package = ReplicatedStorage.Package
-local testFiles = TableUtil.Filter(package:GetChildren(), function(v)
+local testFiles = TableUtil.Filter(ReplicatedStorage.Package:GetChildren(), function(v)
     return string.match(v.Name, ".spec") ~= nil
 end)
 
